d6f495043ef1e12cc01bcdb04c568dfad2f22ac3
Your Name <you@example.com>
2022-03-21 15:38:56 +0100

    HLS react player

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from "react";
+import ReactHlsPlayer from "react-hls-player";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Video player</h1>
-      <video id="videoPlayer" width="650" controls muted="muted" autoplay>
-        <source src="http://localhost:3030/video/1" type="video/mp4" />
-      </video>
+        <h1>Video player</h1>
+        <ReactHlsPlayer
+          src="http://localhost:3030/video"
+          autoPlay={false}
+          controls={true}
+          width="50%"
+          height="auto"
+        />
       </header>
     </div>
   );
